2ddb007f6c347b49df2ee49909fe4c6ec1f3d295
Andrew Gioia <andrewgioia@gmail.com>
2014-04-24 17:27:19 -0400

    Initial blog style to homepage and post

---
+++ app/etc/env/numenor.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+    'app' => array(
+        'environment' => 'local' ),
+
+    'paths' => array(
+        'baseUri' => 'http://numenor.local/geist/',
+        'assetUri' => 'http://numenor.local/geist/',
+        'hostname' => 'numenor.local/' ),
+
+    'cookies' => array(
+        'secure' => TRUE ),
+
+    'database' => array(
+        'password' => '##SQLPASSWORD##' ),
+
+    'profiling' => array(
+        'query' => FALSE
+    ));
