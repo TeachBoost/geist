f90cefc4f55637d41927e0c6e1e7e0aba934e1cc
Mike Gioia <mikegioia@gmail.com>
2014-05-04 17:07:36 -0400

    added RSS feed

--- app/controllers/PostsController.php
+++ app/controllers/PostsController.php
@@ -2,6 +2,10 @@
 
 namespace Controllers;
 
+use \Suin\RSSWriter\Feed as Feed,
+    \Suin\RSSWriter\Channel as Channel,
+    \Suin\RSSWriter\Item as Item;
+
 class PostsController extends \Base\Controller
 {
     public function beforeExecuteRoute()
@@ -14,13 +18,11 @@ class PostsController extends \Base\Controller
     public function showAction()
     {
         // load the params
-        //
         $year = $this->dispatcher->getParam( 'year' );
         $month = $this->dispatcher->getParam( 'month' );
         $slug = $this->dispatcher->getParam( 'slug' );
 
         // find the post by slug
-        //
         $post = \Db\Sql\Posts::getBySlug( $slug );
 
         if ( ! $post )
@@ -31,12 +33,44 @@ class PostsController extends \Base\Controller
         }
 
         // get category name and slug
-        //
         $category = \Db\Sql\Categories::getByID( $post->category_id );
 
         $this->data->post = $post;
         $this->data->category = $category;
         $this->data->pageTitle = $post->title;
         $this->view->pick( 'posts/show' );
+    }
+
+    public function rssAction()
+    {
+        // create the feed
+        $feed = new Feed();
+
+        // create the channel
+        $channel = new Channel();
+        $channel
+            ->title( 'TeachBoost Blog' )
+            ->description(
+                'Teacher development and coaching redefined. '.
+                'Get TeachBoost for your school or district at '.
+                'teachboost.com.' )
+            ->url( $this->config->paths->baseUri )
+            ->appendTo( $feed );
+
+        // get the most recent posts
+        $posts = \Db\Sql\Posts::getActive( 20 );
+
+        foreach ( $posts as $post )
+        {
+            $item = new Item();
+            $item
+                ->title( $post->title )
+                ->description( $post->excerpt )
+                ->url( $post->getPath() )
+                ->appendTo( $channel );
+        }
+
+        echo $feed;
+        exit;
     }
 }
