f0163e73d79b7a02370d1e3a9a500e0a5fcdacdd
Andrew Gioia <andrewgioia@gmail.com>
2014-05-05 12:50:30 -0400

    Mobile media query support for the homepage; js menu support for mobile devices in an elegant hamburger solution

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 /*
- * Show the subscription form */
+ * Subscription Form Toggle */
 $( '#subscribe-button' ).on( 'click', function() {
     $( this ).hide();
     $( '#mc-embedded-subscribe-form' ).show();
@@ -7,11 +7,32 @@ $( '#subscribe-button' ).on( 'click', function() {
 });
 
 /*
- * Build the google search when submitted */
+ * Google Search Box */
 $( '#search-submit' ).on( 'click', function() {
     var url = 'https://www.google.com/#q=site:blog.teachboost.com+';
     var query = $( '#search-query' ).val().replace( ' ', '+' );
     var action = url + query;
     window.location = action;
     return false;
+});
+
+/*
+ * Mobile Menu */
+$( '#mobile-burger' ).on( 'click', function() {
+
+    // get menu state and menu element
+    //
+    var state = $( this ).data( 'state' );
+    var menu = $( '#mobile-menu' );
+
+    // toggle this thing
+    //
+    menu.slideToggle( 300, function() { });
+
+    // adjust the button's data state
+    //
+    ( state == 'off' )
+        ? $( this ).data( 'state', 'on' ).children( 'i' ).addClass( 'fa-rotate-90' )
+        : $( this ).data( 'state', 'off' ).children( 'i' ).removeClass( 'fa-rotate-90' );
+
 });
