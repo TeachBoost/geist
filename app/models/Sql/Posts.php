20c7dbcf18d48aef29c408ef4d06c52b84739e3e
Mike Gioia <mikegioia@gmail.com>
2014-05-06 14:46:58 -0400

    Fixed bug in posts model

--- app/models/Sql/Posts.php
+++ app/models/Sql/Posts.php
@@ -143,8 +143,10 @@ class Posts extends \Base\Model
 
         // process any soundcloud links
         $soundcloudEmbed = sprintf(
-            '<iframe width="100%" height="450" scrolling="no" frameborder="no" '.
+            '<iframe width="%s" height="%s" scrolling="no" frameborder="no" '.
                 'src="%s/%s%s"></iframe>',
+            '100%',
+            '450',
             'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks',
             '$1', // preg variable of cloud ID
             '&amp;auto_play=false&amp;hide_related=false&amp;visual=true' );
