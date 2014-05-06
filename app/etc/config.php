6b7f3dfc3c29aeb6db867de28d210a21e219cf12
Mike Gioia <mikegioia@gmail.com>
2014-05-06 11:19:51 -0400

    changed banner image css

--- app/etc/config.php
+++ app/etc/config.php
@@ -3,7 +3,7 @@
 return array(
     'app' => array(
         'environment' => 'local',
-        'assetVersion' => 2,
+        'assetVersion' => 3,
         'errorReporting' => TRUE,
         // can be 'api' or 'view'
         'responseMode' => 'view',
