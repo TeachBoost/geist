86174453c187336479c9752bd980d9e610b6d3af
Andrew Gioia <andrewgioia@gmail.com>
2014-07-14 10:07:34 -0500

    Adding the testimonial video button to the sideboards

---
+++ app/etc/env/lorien.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+    'app' => array(
+        'environment' => 'local' ),
+
+    'paths' => array(
+        'baseUri' => 'http://lorien.local/geist/',
+        'assetUri' => 'http://lorien.local/geist/',
+        'hostname' => 'lorien.local/' ),
+
+    'cookies' => array(
+        'secure' => TRUE ),
+
+    'database' => array(
+        'password' => '' ),
+
+    'profiling' => array(
+        'query' => FALSE
+    ));
