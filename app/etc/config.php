79e755e2a471e56b5e4ef564d959af58ddcf3e41
Mike Gioia <mikegioia@gmail.com>
2014-07-17 16:38:27 -0400

    Styled tags, incremented asset version

--- app/etc/config.php
+++ app/etc/config.php
@@ -3,7 +3,7 @@
 return array(
     'app' => array(
         'environment' => 'local',
-        'assetVersion' => 6,
+        'assetVersion' => 7,
         'errorReporting' => TRUE,
         // can be 'api' or 'view'
         'responseMode' => 'view',
