8559d07b5c09a033695b0141275024ef68c727ab
Andrew Gioia <andrewgioia@gmail.com>
2014-10-02 11:07:02 -0400

    Updating asset version to 9

--- app/etc/config.php
+++ app/etc/config.php
@@ -3,7 +3,7 @@
 return array(
     'app' => array(
         'environment' => 'local',
-        'assetVersion' => 8,
+        'assetVersion' => 9,
         'errorReporting' => TRUE,
         // can be 'api' or 'view'
         'responseMode' => 'view',
