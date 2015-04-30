72cb8be63e3dfeee25c7f317a54aa5ed64784cfc
Andrew Gioia <andrewgioia@gmail.com>
2015-04-30 16:48:27 -0400

    Updating asset version and build files

--- app/etc/config.php
+++ app/etc/config.php
@@ -3,7 +3,7 @@
 return array(
     'app' => array(
         'environment' => 'local',
-        'assetVersion' => 11,
+        'assetVersion' => 12,
         'errorReporting' => TRUE,
         // can be 'api' or 'view'
         'responseMode' => 'view',
